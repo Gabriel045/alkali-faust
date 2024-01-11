c72f8154b477e9730f3ba8d9abcda8ab1b31e490
gabriel Cabrera <gabriel.cabera@gmail.com>
2024-01-11 11:55:10 -0400

    a

--- components/TestimonialResponsive/index.js
+++
@@ -1,1 +0,0 @@
-export {default as TestimonialResponsive} from './TestimonialResponsive';
--- constants/menus.js
+++
@@ -1,6 +0,0 @@
-export const PRIMARY_LOCATION = 'PRIMARY';
-export const HOW_WE_HELP = 'HOW_WE_HELP';
-export const WHO_WE_HELP = 'WHO_WE_HELP';
-export const WHY_APPLY = 'WHY_APPLY';
-export const RESOURCES = 'RESOURCES';
- 
