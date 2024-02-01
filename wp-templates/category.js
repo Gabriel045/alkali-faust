bbbacf701e062dd68730baabd2eb86bf126bc48e
gabriel Cabrera <gabriel.cabera@gmail.com>
2024-02-01 19:46:46 -0400

    category page test

--- wp-templates/category.js
+++ wp-templates/category.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 import * as MENUS from '../constants/menus';
-import { BlogInfoFragment } from '../fragments/GeneralSettings';
+import {BlogInfoFragment,themeGeneralSettingsFragment} from '../fragments/GeneralSettings';
 import {
   Header,
   Footer,
@@ -14,10 +14,32 @@ import {
 } from '../components';
 
 export default function Component(props) {
+
+  console.log(props);
+  
   const { title: siteTitle, description: siteDescription } =
     props?.data?.generalSettings;
-  const primaryMenu = props?.data?.headerMenuItems?.nodes ?? [];
-  const footerMenu = props?.data?.footerMenuItems?.nodes ?? [];
+  //const primaryMenu = props?.data?.headerMenuItems?.nodes ?? [];
+  //const footerMenu = props?.data?.footerMenuItems?.nodes ?? [];
+
+  const primaryMenu = props?.data.headerMenuItems?.nodes ?? [];
+  const footerMenu = props?.data.footerMenuItems?.nodes ?? [];
+  const footerMenu2 = props?.data.footerMenuItems2?.nodes ?? [];
+  const footerMenu3 = props?.data.footerMenuItems3?.nodes ?? [];
+  const footerMenu4 = props?.data.footerMenuItems4?.nodes ?? [];
+  const logoUrl     = props?.data.themeGeneralSettings?.themeSetting.header.logo?.node.mediaItemUrl
+  const footerTexts = {
+    'title': props?.data.themeGeneralSettings?.themeSetting.footer?.footerTitle,
+    'paragraph': props?.data.themeGeneralSettings?.themeSetting.footer?.footerParagraph
+  }
+  const socialMedia = {
+    'facebook': props?.data.themeGeneralSettings?.themeSetting.socialMedia?.facebook,
+    'instagram': props?.data.themeGeneralSettings?.themeSetting.socialMedia?.instagram,
+    'linkedin': props?.data.themeGeneralSettings?.themeSetting.socialMedia?.linkedin,
+    'twitter': props?.data.themeGeneralSettings?.themeSetting.socialMedia?.x
+
+  }  
+
   const { name, posts } = props.data.nodeByUri;
 
   return (
@@ -27,11 +49,12 @@ export default function Component(props) {
         title={siteTitle}
         description={siteDescription}
         menuItems={primaryMenu}
+        logoUrl={logoUrl}
       />
       <Main>
         <>
           <EntryHeader title={`Category: ${name}`} />
-          <Container>
+          {/*<Container>
             {posts.edges.map((post) => (
               <Post
                 title={post.node.title}
@@ -42,54 +65,69 @@ export default function Component(props) {
                 featuredImage={post.node.featuredImage?.node}
               />
             ))}
-          </Container>
+          </Container>*/}
         </>
       </Main>
-      <Footer title={siteTitle} menuItems={footerMenu} />
+      <Footer footerTexts={footerTexts} socialMedia={socialMedia} footerMenu={footerMenu} footerMenu2={footerMenu2} footerMenu3={footerMenu3} footerMenu4={footerMenu4} />
     </>
   );
 }
 
 Component.query = gql`
   ${BlogInfoFragment}
+  ${themeGeneralSettingsFragment}
   ${NavigationMenu.fragments.entry}
-  ${FeaturedImage.fragments.entry}
   query GetCategoryPage(
-    $uri: String!
-    $headerLocation: MenuLocationEnum
-    $footerLocation: MenuLocationEnum
+    $headerLocation:  MenuLocationEnum
+    $footerLocation:  MenuLocationEnum
+    $footerLocation2: MenuLocationEnum
+    $footerLocation3: MenuLocationEnum
+    $footerLocation4: MenuLocationEnum
   ) {
-    nodeByUri(uri: $uri) {
+    nodeByUri(uri: "blog/about-us") {
       ... on Category {
-        name
         posts {
-          edges {
-            node {
-              id
-              title
-              content
-              date
-              uri
-              ...FeaturedImageFragment
-              author {
-                node {
-                  name
-                }
+          nodes {
+            title
+            featuredImage {
+              node {
+                sourceUrl
               }
             }
+            date
+            link
           }
         }
       }
     }
+
+    themeGeneralSettings{
+      ...themeGeneralSettingsFragment
+    }
     generalSettings {
       ...BlogInfoFragment
     }
-    headerMenuItems: menuItems(where: { location: $headerLocation }) {
+   headerMenuItems: menuItems(where: { location: $headerLocation }) {
       nodes {
         ...NavigationMenuItemFragment
       }
     }
     footerMenuItems: menuItems(where: { location: $footerLocation }) {
+      nodes {
+        ...NavigationMenuItemFragment
+      }
+    }
+    footerMenuItems2: menuItems(where: { location: $footerLocation2 }) {
+      nodes {
+        ...NavigationMenuItemFragment
+      }
+    }
+    footerMenuItems3: menuItems(where: { location: $footerLocation3 }) {
+      nodes {
+        ...NavigationMenuItemFragment
+      }
+    }
+    footerMenuItems4: menuItems(where: { location: $footerLocation4 }) {
       nodes {
         ...NavigationMenuItemFragment
       }
